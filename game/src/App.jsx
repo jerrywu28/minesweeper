2015b2f52f6cae865820768d093da03d77747636
Jerry Wu <wuj828@gmail.com>
2018-11-12 23:29:12 -0800

    Disabled right-click default. Actual right-click functionality WIP, searching for flag symbol to use.

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -16,6 +16,7 @@ class App extends Component {
 
   componentDidMount() {
     this.makeBoard();
+    document.oncontextmenu = (e) => e.preventDefault();
   }
 
   makeBoard() {
--- game/src/Squares.jsx
+++ game/src/Squares.jsx
@@ -4,15 +4,20 @@ import './Styles/Squares.css';
 const Squares = props => {
 
   const clickSquare = e => {
-    console.log(e.target.value);
-    e.target.innerText = e.target.value;
+    if (e.target.value !== '0') {
+      e.target.innerText = e.target.value;
+    }
     e.target.classList.add('left-clicked');
+  }
+
+  const rightClick = e => {
+    console.log(e.target.classList, e.target.value);
   }
 
   return (
     <div>
       {props.row.map((col, colIndex) => {
-        return <button key={colIndex} index={colIndex} value={col} onClick={e => clickSquare(e)}></button>
+        return <button key={colIndex} index={colIndex} value={col} onClick={e => clickSquare(e)} onContextMenu={e => rightClick(e)}></button>
       })}
     </div>
   )
